641ad628264234ae974583cac259a2708c0f44d4
alex <alex@work>
2014-10-13 18:41:41 +0400

    remove other address fields

--- app/overrides/address_fields.rb
+++ app/overrides/address_fields.rb
@@ -1,6 +1,9 @@
 Deface::Override.new(:virtual_path => 'spree/checkout/address/_form',
-                     :remove => 'erb[loud]:contains("address2")')
+                     :remove => 'erb[loud]:contains("address2")',
+                     :name => 'kill address2')
 Deface::Override.new(:virtual_path => 'spree/checkout/address/_form',
-                     :remove => 'erb[loud]:contains("lastname")')
+                     :remove => 'erb[loud]:contains("lastname")',
+                     :name => 'kill lastname')
 Deface::Override.new(:virtual_path => 'spree/checkout/address/_form',
-                     :remove => 'erb[loud]:contains("zipcode")')
+                     :remove => 'erb[loud]:contains("zipcode")',
+                     :name => 'kill zipcode')
